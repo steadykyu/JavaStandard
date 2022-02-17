6746184bd9dee152e96d7ddea5de1402592024bf
steadykyu <awakekyu1@google.com>
2022-02-18 01:20:47 +0900

    문제 pdf추가2

--- src/ch1startjava/Hello.java
+++ src/ch1startjava/Hello.java
@@ -1,8 +1,12 @@
 package ch1startjava;
 
+import static java.lang.Math.exp;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+       byte b = 10;
+       b= (byte)1000;
+        System.out.println(b);
         //출력
     }
 }
